81052e91ef427d954cf3a185117c66aed0fa5fc4
vityha <viktor.povazhuk@ucu.edu.ua>
2022-10-12 20:34:28 +0300

    1. Functions decomposition. 2. Support of 2 script run methods

--- myshell/src/main.cpp
+++ myshell/src/main.cpp
@@ -10,80 +10,102 @@
 #include <sys/wait.h>
 #include <memory>
 #include <fstream>
+#include <filesystem>
 
 #include "options_parser.h"
 #include "errors.h"
 
+namespace fs = std::filesystem;
+
 extern char **environ;
 
-int builtin_command() {
-    return 0;
+void run_builtin_command(std::vector<std::string> &args) {
+
 }
 
-int exec_com_line(const std::string &com_line) {
-//    відкинути пробіли і решіточку
+std::vector<std::string> parse_com_line(const std::string &com_line) {
+    // remove leading spaces and comment
     const auto str_begin = com_line.find_first_not_of(' ');
     const auto str_end = com_line.find_first_of('#');
     const auto str_range = str_end - str_begin;
     std::string clean_com_line = com_line.substr(str_begin, str_range);
-    std::cout << clean_com_line << '\n';
-//    розбити по пробілах
+//    std::cout << clean_com_line << '\n';
+
+    // split by space
     std::stringstream streamData(clean_com_line);
     std::vector<std::string> args;
     std::string value;
     while (std::getline(streamData, value, ' ')) {
         args.push_back(value);
     }
-    for (auto &val: args) {
-        std::cout << val << std::endl;
-    }
-//    перевірка на вбудовані команди - виконати
-    if (args[0] == "mycat") {
-        builtin_command();
-    }
-//    ні - ім’я команди - викликає форк-екзек, формувати блоки в елсе
-    else {
-        pid_t pid = fork();
+//    for (auto &val: args) {
+//        std::cout << val << std::endl;
+//    }
 
-        while (pid == -1) {
-            if (errno == EINTR) {
-                pid = fork();
-                continue;
-            } else {
-                perror("Fork failed");
-                exit(errno);
-            }
-        }
+    return std::move(args);
+}
 
-        if (pid != 0) {
-            int status;
-            waitpid(pid, &status, 0);
-            // TODO: fix incorrect status. What is status at all?
-            if (status != 0) {
-                exit(500);
-            }
-        }
-        else {
-            std::string child_name = args[0];
+void run_outer_command(std::vector<std::string> &args) {
+    pid_t pid = fork();
 
-            std::vector<const char*> args_for_exec;
-            for (const auto &str: args) {
-                args_for_exec.push_back(str.c_str());
-            }
-            args_for_exec.push_back(nullptr);
-
-            auto path_ptr = getenv("PATH");
-            string path_var;
-            if(path_ptr != nullptr)
-                path_var = path_ptr;
-            path_var += ":.";
-            setenv("PATH", path_var.c_str(), 1);
-
-            execvp(child_name.c_str(), const_cast<char *const *>(args_for_exec.data()));
-
-            perror("Execve failed");
+    while (pid == -1) {
+        if (errno == EINTR) {
+            pid = fork();
+            continue;
+        } else {
+            perror("Fork failed");
             exit(errno);
         }
+    }
+
+    if (pid != 0) {
+        int status;
+        waitpid(pid, &status, 0);
+        // TODO:
+        //  1. What is status at all?
+        //  2. What to do is command was incorrect: continue or exit? Continue, but main process prints info before '$'.
+    }
+    else {
+        std::string child_name = args[0];
+
+        std::vector<const char*> args_for_exec;
+        for (const auto &str: args) {
+            args_for_exec.push_back(str.c_str());
+        }
+        args_for_exec.push_back(nullptr);
+
+        auto path_ptr = getenv("PATH");
+        string path_var;
+        if(path_ptr != nullptr)
+            path_var = path_ptr;
+        path_var += ":.";
+        setenv("PATH", path_var.c_str(), 1);
+
+        execvp(child_name.c_str(), const_cast<char *const *>(args_for_exec.data()));
+
+        // bug of CLion: reads firstly from cout, then from cerr
+        // even if buffers are flushed
+        perror("Execve failed");
+        exit(errno);
+    }
+}
+
+void exec_com_line(const std::string &com_line) {
+    std::vector<std::string> args = parse_com_line(com_line);
+    // check for in-built command
+    if (args[0] == "mycat") {
+        run_builtin_command(args);
+    }
+    // check for script
+    else if (args.size() == 1 && fs::path{args[0]}.extension() == ".msh" && fs::exists(fs::path{args[0]})) {
+        fs::path script_path{args[0]};
+        args.push_back(args[0]);
+        args[0] = "myshell";
+        run_outer_command(args);
+    }
+    // run fork-exec
+    else {
+        run_outer_command(args);
     }
 }
 
@@ -114,9 +136,9 @@ int main(int argc, char* argv[]) {
     }
 
     while (true) {
+        // TODO: add wd with created pwd() function
         std::cout << "$ ";
 
-        // TODO: ask about multiline commands
         std::string com_line;
         std::getline(std::cin, com_line);
 
